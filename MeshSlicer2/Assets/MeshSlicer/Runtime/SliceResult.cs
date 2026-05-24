2d256e818b3e20cc2102a94477448130f383c495
Keijiro Takahashi <keijiro@gmail.com>
2026-05-24 21:45:57 +0900

    Add MeshSlicer2

---
+++ MeshSlicer2/Assets/MeshSlicer/Runtime/SliceResult.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace MeshSlicer
+{
+    public readonly struct SliceResult
+    {
+        public readonly Mesh Positive;
+        public readonly Mesh Negative;
+
+        public SliceResult(Mesh positive, Mesh negative)
+        {
+            Positive = positive;
+            Negative = negative;
+        }
+    }
+}
